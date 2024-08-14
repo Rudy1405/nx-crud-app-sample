e4fe10d5bdaed9947f871989c3b023dbb81cc263
Rudy1405 <rudy1405@hotmail.com>
2024-08-14 13:45:59 -0600

    instructions for update

--- apps/my-app/src/main.ts
+++ apps/my-app/src/main.ts
@@ -36,3 +36,26 @@ const newAppConfig = {
 bootstrapApplication(AppComponent, newAppConfig).catch((err) =>
   console.error(err)
 );
+
+
+/**
+ * Cliente usa NX para monorepo
+ * 
+ * 1)
+ * mono lib de componentes com[patridos], usar diff proyects y usar librerias similares
+ * falta el folder de lib compartidas el /libs y la /shared que ahi se tenga la ui o los datos compartidos y el storage
+ * revisa el nx.dev documentacion angular mono repo
+ * tomar mi shared creatdo y hacerlo a la altura de /apps 
+ * 
+ * 2) Crear formulario en el modal que sea dinamico osea es un formulario dinamico 
+ *    que sirva para agregar categoria,  Angular FormBuilder 
+ * 
+ * 
+ * 3)  boton para editar en el datagrid y abra el formulario y editarlo 
+ * 4) boton para eliminar 
+ * 5) mi componente dummy que esl el datagrid en los botones manda un EMIT a sus padres osea el category
+ * 
+ * TIP: al usar los shared en el import the esos shared en category  el tscofig.base.json ahora tiene el atrr paths y ahi se ponen las nuevas rutas  
+ * 
+ * 
+ */
